1b992ddab7aa170545ba34a4ddee7e961e7912c3
imasarnago <imanolsarnago@gmail.com>
2024-05-28 21:11:37 -0300

    agrego cositas jsjs

--- funciones.s
+++ funciones.s
@@ -8,7 +8,7 @@
 // (x,y) esta en la direccion:   Dirección de inicio + 4 * [x + (y * 640)]  <--- FORMULA DADA EN CLASE
 // fun dibujar_pixel(x,y,color) regs:(X1, X2, X7) desc: pixel en (x, y) del color color
 // x0 contiene la direccion base del framebuffer
-
+             // --- HECHA POR IMANOL
 dibujar_pixel:
     MOV X16, SCREEN_WIDTH                // X16 = SCREEN_WIDTH
     MUL X16, X2, X16                     // X16 = X2 * X16 = y * 640
@@ -20,7 +20,7 @@ dibujar_pixel:
 RET
 
 
-
+             // --- HECHA POR IMANOL    
 /* La idea es que quiero dibujar un cuadrado. No solicitare 4 puntos, sino 2. Uno en la esquina opuesta
     del otro. Asi, los calculos de las otras esquinas es trivial. */
 
@@ -32,8 +32,8 @@ RET
 // X6 - Coordenada Y2 (superior derecho)
 // X7 - Color (ARGB)
 dibujarCuadrado:
-    mov x1, x3               // Guardar X1 en x1
-    mov x2, x4               // Guardar Y1 en x2
+    mov x1, x3               // Guardar X1 (registro) en x1 (coord. horizontal)
+    mov x2, x4               // Guardar Y1 (registro) en x2 (coord. vertical), etc...
     mov x8, x5               // Guardar X2 en x8
     mov x9, x6               // Guardar Y2 en x9
 
@@ -41,10 +41,10 @@ bucleFila:
     cmp x2, x9               // Comparar Y1 con Y2
     bgt finCuadrado          // Si Y1 > Y2, terminar
 
-    mov x1, x3               // Reiniciar X1 para la nueva fila
+    mov x1, x3               // Reiniciar X1 par a la nueva fila
 bucleColumna:
     cmp x1, x8               // Comparar X1 con X2
-    bgt siguienteFila        // Si X1 > X2, pasar a la siguiente fila
+    bgt siguienteFila        // Si X1 > X2 pasa a la siguiente fila
 
     // Pintar el píxel actual
     mov x10, x1              // Copiar X1 a x10
@@ -52,8 +52,8 @@ bucleColumna:
     bl dibujar_pixel         // Llamar a dibujar_pixel
 
     // Siguiente columna
-    add x1, x1, #1           // X1++
-    b bucleColumna           // Repetir para la siguiente columna
+    add x1, x1, #1           // "X1++" 
+    b bucleColumna           // Repetir para la siguiente col.
 
 siguienteFila:
     // Siguiente fila
