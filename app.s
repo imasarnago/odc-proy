b985811dcf96071f178bcaf06073e85f047b0e0f
imasarnago <imanolsarnago@gmail.com>
2024-06-04 20:34:25 -0300

    Agrego manzanas al arbol

--- app.s
+++ app.s
@@ -19,9 +19,9 @@ main:
 
     // Coordenadas de las esquinas opuestas del cuadrado
     // Establecer las coordenadas del cuadrado y el color:
-    MOV X3, 569              // x1 (esquina inferior izquierda)
+    MOV X3, 549              // x1 (esquina inferior izquierda)
     MOV X4, 165              // y1 (esquina inferior izquierda)
-    MOV X5, 579              // x2 (esquina superior derecha)
+    MOV X5, 559              // x2 (esquina superior derecha)
     MOV X6, 172              // y2 (esquina superior derecha)
     LDR X7, =COLOR_FUEGO_0    // color (cargar el color desde una dirección de memoria)
     BL dibujarCuadrado        // Llamar a la función dibujarCuadrado
@@ -30,16 +30,16 @@ main:
 
 
 
-	MOV X3, 549            
+	MOV X3, 529            
     MOV X4, 172              
-    MOV X5, 559              
+    MOV X5, 539              
     MOV X6, 179            
     LDR X7, =COLOR_FUEGO_0   
     BL dibujarCuadrado        
 
-	MOV X3, 549              
+	MOV X3, 529              
     MOV X4, 142              
-    MOV X5, 559              
+    MOV X5, 539              
     MOV X6, 149              
     LDR X7, =COLOR_FUEGO_0   
     BL dibujarCuadrado       
@@ -56,19 +56,48 @@ main:
     LDR X7, =COLOR_ARBOL    
     BL dibujarCuadrado      
  
-	MOV X3, 0  
-    MOV X4, 298   
-    MOV X5, 639 
-    MOV X6, 479 
-    LDR X7, =COLOR_PASTO    
-    BL dibujarCuadrado    
-            // DEJO DE DIBUJAR EL TRONCO DEL ARBOL
 
+            
+            // DEJO DE DIBUJAR EL TRONCO DEL ARBOL
             // EMPIEZO A DIBUJAR LA COPA DEL ARBOL
 
+	MOV X3, 470  
+    MOV X4, 15   
+    MOV X5, 639 
+    MOV X6, 129 
+    LDR X7, =COLOR_COPA    
+    BL dibujarCuadrado  
+
+	MOV X3, 465  
+    MOV X4, 24  
+    MOV X5, 484 
+    MOV X6, 43 
+    LDR X7, =COLOR_MANZANA    
+    BL dibujarCuadrado      
+
+	MOV X3, 622
+    MOV X4, 61  
+    MOV X5, 638 
+    MOV X6, 85
+    LDR X7, =COLOR_MANZANA    
+    BL dibujarCuadrado  
+
+    MOV X3, 555  
+    MOV X4, 100  
+    MOV X5, 579 
+    MOV X6, 120 
+    LDR X7, =COLOR_MANZANA    
+    BL dibujarCuadrado  
 
 
-
+            // DIBUJO EL PASTO
+	MOV X3, 0  
+    MOV X4, 298   
+    MOV X5, 640 
+    MOV X6, 483 
+    LDR X7, =COLOR_PASTO    
+    BL dibujarCuadrado    
+  
 
 
 
